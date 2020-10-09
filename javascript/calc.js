accabb5fb2afc6111d1e18605a9e4029871a6d88
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2020-10-09 17:14:38 +0530

    initial commit

---
+++ javascript/calc.js
@@ -0,0 +1,4 @@
+var a = prompt("Enter first number");
+var b = prompt("Enter second number");
+var c = Number(a) + Number(b)
+alert(" the sum of these two numbers is" + c);
---
+++ javascript/if-else.js
@@ -0,0 +1,10 @@
+//take input from the user
+name = prompt("Enter a name");
+
+//apply the if-else condition
+
+if(name == "Mathew"){
+    alert("Hii Mathew !!")
+}else{
+    alert("Umm...I dont know you")
+}
