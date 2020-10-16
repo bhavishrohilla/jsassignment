3de40abea2f159b5e128f15172f96aee750e1546
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2020-10-16 14:51:38 +0530

    added files

---
+++ javascript/else if.js
@@ -0,0 +1,14 @@
+// taking the input from the user
+
+name = prompt("Enter a name");
+
+// apply the conditions now
+if(name === "Billy"){
+    alert("Hii Billy!!");
+}
+else if(name === "Susy"){
+    alert("Hii Susy !!");
+}
+else{
+    alert("NO USER DETECTED !!")
+}
