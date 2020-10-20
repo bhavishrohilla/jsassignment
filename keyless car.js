8e5297924fd53b667c7eab79205e2e4b20211d34
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2020-10-20 16:20:58 +0530

    exercise keyless car

---
+++ keyless car.js
@@ -0,0 +1,10 @@
+var age = prompt("Mention you age=");
+if(Number(age)<18){
+    alert("Sorry, you are too young to drive this car.Powering off");
+}
+else if(Number(age) === 18){
+    alert("Congratulations on your first year of driving. Enjoy the ride!");
+}
+else if(Number(age)>18) {
+    alert("Powering On. Enjoy the ride!");
+}
